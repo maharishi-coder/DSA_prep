bf956369b1e0c55bed2b5def2faf4f68d90d6ac6
RISHI RAJ <rishiraj619march@gmail.com>
2022-03-17 17:56:18 +0530

    Sort an array and stack using recursion

---
+++ Recursion/SortanArrayusingRecursion.cpp
@@ -0,0 +1,82 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h> 
+ 
+using namespace std;
+ 
+typedef long long ll;
+#define int ll
+typedef long double ld;
+const ll INF=1e18;
+const ll M=1e9+7;
+const ll MOD = 998244353;
+double eps = 1e-12;
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define forn(i,e) for(ll i = 0; i < e; i++)
+#define forsn(i,s,e) for(ll i = s; i < e; i++)
+#define rforn(i,s) for(ll i = s; i >= 0; i--)
+#define rforsn(i,s,e) for(ll i = s; i >= e; i--)
+#define ln "\n"
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define INF 2e18
+#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+void insert(vector<int>&arr,int temp){
+    if(arr.size()==0 || arr[arr.size()-1]<=temp){
+        arr.push_back(temp);
+        return;
+    } 
+    int val=arr[arr.size()-1];
+    arr.pop_back();
+    insert(arr,temp);
+    arr.push_back(val);
+
+}
+
+void sort(vector<int>&arr){
+    if(arr.size()==1){
+        return;
+    }
+    int temp=arr[arr.size()-1];
+    arr.pop_back();
+    sort(arr);
+    insert(arr,temp);
+
+}
+
+void solve(){
+    int n;
+    cin>>n;
+    vector<int>arr(n);
+    forn(i,n)cin>>arr[i];
+    sort(arr);
+    for(auto x:arr){
+        cout<<x<<" ";
+    }
+    cout<<ln;
+}
+
+signed main()
+{
+//FUCK the rating, Just learn & grow after every contest!!! BEST of luck maharishi...you can do it..believe that...
+ fast_cin();
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+   freopen("error.txt", "w", stderr);
+#endif
+
+ ll t=1;
+ cin >> t;
+ while(t--) {
+     solve();
+ }
+cerr << "Time : " << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms";
+ return 0;
+}
